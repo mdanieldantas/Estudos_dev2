39c328743cec9b3c26a18da535ca75dcee46ec3d
Daniel Dantas Mac <contatomarcosdgomes@gmail.com>
2023-02-14 20:24:59 -0300

    estudando livro Js

---
+++ Logica-de-programacao-e-algoritmos-com-javascript-novatec-master/exercicios livroJs/exer1.js
@@ -0,0 +1,38 @@
+
+
+
+let valor = Number(prompt('Insira o valor do Jantar: '))
+let dezPorcento = valor / 10
+let valorTotal = dezPorcento + valor
+alert('O valor total do jantar foi de: ' + valorTotal + '\nO valor cobrado de dez porcento é de: ' + dezPorcento)
+
+/*
+let valorJantar = Number(prompt('valor do jantar: '))
+let dezPorcento = valorJantar * 0.10;
+let total = valorJantar + dezPorcento
+alert('Taxa Garçom R$: ' + dezPorcento.toFixed(2) + '\nTotal R$: ' + total.toFixed(2))
+
+
+
+let num1 = Number(prompt('1º Número: '));
+let num2 = Number(prompt('2º Número: '));
+let soma = num1 + num2;
+alert(soma)
+
+const a = prompt('Coloque um numero para ser multiplicado ao dobro: ')
+alert('O dobro deste número é: ' + a * 2)
+
+var a = "20";
+var b = a * 2; // b = 40;
+var c = a / 2; // c = 10;
+var d = a - 2; // d = 18
+var e = parseInt(a) + 2; // e = 202 ???
+console.log("e: " + e); // exibe o valor de uma variável
+alert(a)
+
+var fruta = "Banana";
+var preco = 3.50;
+var levar = true;
+var novoValor;
+alert(fruta + " " + preco + " " + levar + " " + novoValor)
+*/
