f061f1e75db949333869a7915ba5878915120268
Daniel Dantas Mac <contatomarcosdgomes@gmail.com>
2023-03-01 17:19:34 -0300

    tip calculator

---
+++ JsEm7/Calculadora Tip/index.js
@@ -0,0 +1,14 @@
+function update() {
+  let bill = Number(document.getElementById('yourBill').value)
+  let tipPercent = document.getElementById('tipInput').value
+  let split = document.getElementById('splitInput').value
+  console.log(bill, tipPercent, split)
+
+  let tipValue = bill * (tipPercent / 100)
+  let billTotal = bill + tipValue
+  let billEach = billTotal / split
+
+  document.getElementById('tipPercent').innerHTML = tipPercent + ' %'
+  document.getElementById('tipValue').innerHTML = '$ ' + tipValue
+}
+
