06235863730e5f572b9e0abb88beb1c582e533ae
Daniel Dantas Mac <contatomarcosdgomes@gmail.com>
2023-03-03 14:25:42 -0300

    pick color

--- JsEm7/Calculadora Tip/index.js
+++ JsEm7/Calculadora Tip/index.js
@@ -1,3 +1,14 @@
+function formatMoney(value) {
+  value = Math.ceil(value * 100) / 100
+  value = value.toFixed(2)
+  return '$ ' + value
+}
+
+function formatSplit(value) {
+  if (value == 1) return value + ' Pessoa'
+  return value + ' Pessoas'
+}
+
 function update() {
   let bill = Number(document.getElementById('yourBill').value)
   let tipPercent = document.getElementById('tipInput').value
@@ -9,9 +20,9 @@ function update() {
   let billEach = billTotal / split
 
   document.getElementById('tipPercent').innerHTML = tipPercent + ' %'
-  document.getElementById('tipValue').innerHTML = '$ ' + tipValue
-  document.getElementById('totalWithTip').innerHTML = '$ ' + billTotal
-  document.getElementById('splitValue').innerHTML = split
-  document.getElementById('billEach').innerHTML = '$ ' + billEach
+  document.getElementById('tipValue').innerHTML = formatMoney(tipValue)
+  document.getElementById('totalWithTip').innerHTML = formatMoney(billTotal)
+  document.getElementById('splitValue').innerHTML = formatSplit(split)
+  document.getElementById('billEach').innerHTML = formatMoney(billTotal / split)
 }
 
---
+++ JsEm7/Troque_A_Cor/index.js
@@ -0,0 +1,32 @@
+// entrada
+const colors = ['Red', 'Blue', 'Yellow', 'Purple', 'Green']
+
+const btn = document.getElementById('btn')
+
+
+
+
+//processamento
+
+
+//saída
+
+
+
+
+/*
+const colors = ['Red', 'Blue', 'Yellow', 'Purple', 'Green']
+
+const btn = document.getElementById('btn')
+
+const color = document.querySelector('.color')
+btn.addEventListener('click', function () {
+  const randomColor = getRandomColor()
+  document.body.style.background = colors[randomColor]
+  //console.log(randomColor)
+})
+
+function getRandomColor() {
+  return Math.floor(Math.random() * colors.length)
+}
+*/
