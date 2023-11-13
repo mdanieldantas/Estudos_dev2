819a67b2890492003809643a3b181c0756d5babd
M Daniel  Dantas <contatomarcosdgomes@gmail.com>
2023-11-13 10:49:52 -0300

    tips code

--- GustavoGuanabaraCursoEmVideo/javaScript/aula9.js
+++ GustavoGuanabaraCursoEmVideo/javaScript/aula9.js
@@ -1,1 +1,7 @@
-window.document.write("Olá mundo!");
+//window.document.write("Olá mundo!");
+var p1 = document.getElementsByTagName("p")[0];
+window.document.write("Está escrito assim: " + p1.innerText);
+p1.style.color = "blue"
+
+var d = window.document.getElementById("")
+d.style.background = "green"
---
+++ oneBiteCode/javaScriptModulo2/array-aula 32 - ou 2/array.js
@@ -0,0 +1,7 @@
+const listaDeCompras = []
+listaDeCompras[0] = "Arroz"
+listaDeCompras[1] = "Feijão"
+listaDeCompras[2] = "Farinha de Trigo"
+listaDeCompras[3] = "Leite"
+listaDeCompras[4] = "Pão integral"
+console.log(listaDeCompras)
