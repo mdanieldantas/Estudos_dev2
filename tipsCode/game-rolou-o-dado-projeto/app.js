383c842ba1757d416d055f407d90329429543c2b
M Daniel  Dantas <contatomarcosdgomes@gmail.com>
2023-12-05 16:54:15 -0300

    Reorganizando arquivos

---
+++ tipsCode/game-rolou-o-dado-projeto/app.js
@@ -0,0 +1,29 @@
+
+document.getElementById(`roll-button`).addEventListener(`click`, function () {
+
+  let randomNumber1 = Math.floor(Math.random() * 6) + 1
+
+  let randomDiceImage = `dice` + randomNumber1 + `.png`
+
+  let randomImageSource = `img/` + randomDiceImage
+
+  let image1 = document.querySelectorAll(`img`)[0]
+
+  image1.setAttribute(`src`, randomImageSource)
+
+
+
+  let randomNumber2 = Math.floor(Math.random() * 6) + 1
+
+  let randomImageSource2 = `img/dice` + randomNumber2 + `.png`
+
+  document.querySelectorAll(`img`)[1].setAttribute(`src`, randomImageSource2)
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector(`h1`).innerHTML = `🏆 Jogador 1 ganhou!`;
+  } else if (randomNumber2 > randomNumber1) {
+    document.querySelector(`h1`).innerHTML = `🏆 Jogador 2 ganhou!`;
+  } else {
+    document.querySelector(`h1`).innerHTML = `É um empate!`
+  }
+})
